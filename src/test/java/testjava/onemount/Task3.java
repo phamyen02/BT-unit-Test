8304a6004f4ea5ffc4603f73688879b5c6ca393b
yenpt18_onemount <yenpt18@onemount.com>
2021-07-23 12:22:57 +0700

    TestJava_YenPT18

--- src/test/java/testjava/onemount/Task3.java
+++ src/test/java/testjava/onemount/Task3.java
@@ -24,6 +24,7 @@ public class Task3 {
         }
         return output.toString().trim();
     }
+
     public static void main(String[] args) {
         Task3 task3 = new Task3();
         task3.readString();
